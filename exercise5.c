664e5a2b52dc5b04ed6bd3a46eccd5c42ba69c20
Sajal Gupta <70312106+guptasajal411@users.noreply.github.com>
2020-09-09 10:34:45 +0530

    Update exercise5.c

--- exercise5.c
+++ exercise5.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
-void arrayRev(int arr())
+void arrayRev(int arr[])
 {
-    void swap(int a, int b)
-    {
-        int temp = a;
-        a = b;
-        b = temp;
-    }
-
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 3; i++)
     {
         /* code */
         int hello = arr[i];
-        arr[i] = arr[7 - i];
-        arr[7 - i] = hello;
+        arr[i] = arr[6 - i];
+        arr[6 - i] = hello;
     }
+
+    printf("Array after reversal : ");
 
     for (int j = 0; j < 7; j++)
     {
         /* code */
-        printf("%d", arr(j));
+        printf("%d ", arr[j]);
     }
+    printf("\n\n");
+    
 }
 
 int main()
 {
     /* code */
     int array1[] = {1, 2, 3, 4, 5, 6, 67};
+    printf("\n\n");
+    printf("Array before reversal : ");
+    for (int i = 0; i < 7; i++)
+    {
+        /* code */
+        printf("%d ", array1[i]);
+    }
+    printf("\n\n");
+    arrayRev(array1);
 
     return 0;
 }
